02c157c20888342c1510f7664c8dfbdfdbcd71c9
Hassen <h.hassen.malek@gmail.com>
2024-06-27 10:20:49 +0200

    creation dossier Front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
+  );
+};
+
+export default App;
