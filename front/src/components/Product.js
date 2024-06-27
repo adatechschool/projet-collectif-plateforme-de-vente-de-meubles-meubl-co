7b53a717eb2828dbea69e3ee8577995a21a6d283
Hassen <h.hassen.malek@gmail.com>
2024-06-27 16:48:47 +0200

    mise a jour des porps

--- front/src/components/Product.js
+++ front/src/components/Product.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Product = ({product}) => {
-    
-    
-    return (
-        <div>
-            <p>{product.name}</p>
-            <p>{product.type}</p>
-        </div>
-    );
+const Product = ({ product }) => {
+  return (
+    <div>
+      <p>Name: {product.name}</p>
+      <p>Type: {product.type}</p>
+      <p>Price: {product.price}</p>
+    </div>
+  );
 };
 
 export default Product;
+
+
