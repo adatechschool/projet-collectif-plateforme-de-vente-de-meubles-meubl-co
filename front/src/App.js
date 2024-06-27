687d7491dd8a112cc1fda6bbfefe17d2b21ab679
cindylecat <cindylecat5@gmail.com>
2024-06-27 11:51:57 +0200

    creation du premier router

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Header/>
-    </React.Fragment>
+    <Router>
+      <Routes>
+        <Route path='/' element = {<Home/>}/> 
+          
+        
+      </Routes>
+    </Router>
   );
 };
 
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -2,26 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Link to = "/"><Navbar.Brand>Meubl&Co</Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link href="#link">Panier</Nav.Link>
+            <NavDropdown title="Mon Compte" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Mes Commandes</NavDropdown.Item>
+              
+            
+              <NavDropdown.Item href="#action/3.3">Mon Profil</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Déconnexion
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
