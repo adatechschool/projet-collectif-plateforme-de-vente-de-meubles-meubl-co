9c7e7dff8cf76dab45767e306b8ba55c92683fa7
cindylecat <cindylecat5@gmail.com>
2024-06-27 15:15:11 +0200

    ajout tableau produits

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import Header from '../components/Header';
+import Product from './Produit';
+
 
 const Home = () => {
+    const meubles = [
+        { id: 1, name: 'Table', type: 'Dining', price: 150 },
+        // { id: 2, name: 'Chair', type: 'Office', price: 80 },
+        // { id: 3, name: 'Sofa', type: 'Living Room', price: 300 },
+        // { id: 4, name: 'Bed', type: 'Bedroom', price: 400 },    
+    ]
     return (
         <div>
             <Header/>
+            <Product></Product>
+            <p> <strong>nom :</strong>{meubles[0].name}</p>
         </div>
+
     );
+
 };
 
 export default Home;
