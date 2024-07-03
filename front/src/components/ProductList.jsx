afed025ee66e9982c3c20e66a182dbdba0cac0eb
Bertrand MARIE <photomb09@gmail.com>
2024-07-03 15:09:52 +0200

    modif design front

--- front/src/components/ProductList.jsx
+++ front/src/components/ProductList.jsx
@@ -1,12 +1,13 @@
  // Importe le composant Product depuis le fichier Product.js
+
 import Product from "./Product";
 import { useState, useEffect } from "react";
 
-//recuperation de l'API de synfony
+//recuperation de l'API de symfony
 const Fetch = () => {
   const [supplies, setSupplies] = useState([]);
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/supply")
+    fetch("http://localhost:8000/api/supply")
       .then((res) => { 
         return res.json();
   })
@@ -16,20 +17,6 @@ const Fetch = () => {
   });
 }, []);
 
-
-// const meubles = [ 
-//   { id: 1, name: 'Canapé 6 places', type: 'Main room', price: 150 , img :"https://www.mobilierdefrance.com/36115/canape-d-angle-panorama.jpg"},
-//   { id: 2, name: 'Lit king size', type: 'Bed room', price: 450 , img :"https://cdn.conforama.fr/prod/product/image/42f6/G_CNF_I65582572_B.jpeg"},
-//   { id: 2, name: 'Table', type: 'Kitchen', price: 80, img :"https://blog.home-design.schmidt/wp-content/uploads/2021/10/4_3-SCH_CUISINE_PanneauxRecycles_4472.jpg" },
-//   { id: 2, name: 'Chair', type: 'Office', price: 300 , img :"https://kayumeuble.com/4210-large_default/fauteuil-tissu-jaune-casita-hasf1citmou.jpg"},
-//   { id: 2, name: 'Table chevet', type: 'Bed room', price: 50, img :"https://www.massivmoebel24.fr/images/product_images/popup_images/09-680-772-0087-03.jpg" }
-
-// ]
-
-// const Produit = () => {
-//   const products = []; 
-
-  // Boucle for traditionnelle pour parcourir le tableau meubles
   return (
     <div>
       
