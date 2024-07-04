5204fcadaf1a1fbe0a12d9019f2ee2cfcce506f0
Bertrand MARIE <photomb09@gmail.com>
2024-07-04 02:56:00 +0200

    add object in BACK - some Routes

--- BACK/src/Controller/api/SupplyController.php
+++ BACK/src/Controller/api/SupplyController.php
@@ -18,20 +18,20 @@ class SupplyController extends AbstractController
         ]);
     }
 
-    #[Route('/api/supply/{id}')]
-    public function show(MeubleEcoRepository $repository, int $id) //ici on ne traite que l'information id
+    #[Route('/api/supply/product/{id}')]
+    public function idOne(MeubleEcoRepository $repository, int $id) //ici on ne traite que l'information id
     {
         $furniture = $repository->findOneBy(['id' => $id]);
         //dd($furniture); //var_dump $furniture
         return $this -> json($furniture, 200, [], [
-            'groups' => ['supply.show']
+            'groups' => ['supply.idOne']
         ]);
     }
 
     #[Route('/api/supply/{category}')]
     public function category(MeubleEcoRepository $repository, string $category) //ici on ne traite que l'information category
     {
-        $furniture = $repository->findOneBy(['category' => $category]);
+        $furniture = $repository->findBy(['category' => $category]);
         //dd($furniture); //var_dump $furniture
         return $this -> json($furniture, 200, [], [
             'groups' => ['supply.category']
